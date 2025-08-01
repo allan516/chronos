834882bced0844704c6365ad1a177d0dfa640913
Allan <allan.mendes0976@gmail.com>
2025-08-01 12:29:27 -0300

    feat: create dark and light themes

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,22 +1,68 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import { useState, useEffect } from "react";
+
 import styles from "./styles.module.css";
 
+type AvailableThemes = "dark" | "light";
+
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>("dark");
+
+  useEffect(() => {
+    console.log("Theme mudou " + theme);
+    document.documentElement.setAttribute("data-theme", theme);
+    return () => {
+      console.log("Função de clean up");
+    };
+  }, [theme]);
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) {
+    event.preventDefault();
+
+    setTheme((prevTheme) => {
+      const nextTheme = prevTheme === "dark" ? "light" : "dark";
+      return nextTheme;
+    });
+  }
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href="#">
+      <h1>{theme}</h1>
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ir para a Home"
+        title="Ir para a Home"
+      >
         <HouseIcon />
       </a>
 
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ver Histórico"
+        title="Ver Histórico"
+      >
         <HistoryIcon />
       </a>
 
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Configurções"
+        title="Configurções"
+      >
         <SettingsIcon />
       </a>
 
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Mudar Tema"
+        title="Mudar Tema"
+        onClick={handleThemeChange}
+      >
         <SunIcon />
       </a>
     </nav>
