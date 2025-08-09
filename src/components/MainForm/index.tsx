0d39a1da8ce531b7324bb5fc1abecea2d0d5cd51
Allan <allan.mendes0976@gmail.com>
2025-08-09 12:01:57 -0300

    feat: add react-toastify

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -9,6 +9,8 @@ import { getNextCycle } from "../../utils/getNextCycle";
 import { getNextCycleType } from "../../utils/getNextCycleType";
 import { TaskAcionTypes } from "../../contexts/TaskContext/taskActions";
 import { Tips } from "../Tips";
+import { toast } from "react-toastify";
+import { showMessage } from "../../adapters/showMessage";
 
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
@@ -19,13 +21,14 @@ export function MainForm() {
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    showMessage.dismiss();
 
     if (taskNameInput.current === null) return;
 
     const taskName = taskNameInput.current.value.trim();
 
     if (!taskName) {
-      alert("Digite o nome da tarefa.");
+      showMessage.warn("Digite o nome da tarefa.");
       return;
     }
 
@@ -40,9 +43,12 @@ export function MainForm() {
     };
 
     dispatch({ type: TaskAcionTypes.START_TASK, payload: newTask });
+    showMessage.success("Tarefa iniciada");
   }
 
   function handleInterruptTask() {
+    showMessage.dismiss();
+    showMessage.error("Tarefa interrompida");
     dispatch({ type: TaskAcionTypes.INTERRUPT_TASK });
   }
 
